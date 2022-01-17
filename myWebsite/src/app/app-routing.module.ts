0868f898fd2cb7de4caa711d8e0af96978e836e6
johnparke20 <parkejo1@msu.edu>
2022-01-17 10:01:54 -0500

    added routing

--- myWebsite/src/app/app-routing.module.ts
+++ myWebsite/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
